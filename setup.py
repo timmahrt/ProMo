33f002b3e6a4635e8e3ea397af2b9cb92edb537c
Tim Mahrt <timmahrt@gmail.com>
2017-01-15 14:43:59 +0100

    DOCUMENTATION: Some minor changes to setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ Created on Aug 29, 2014
 
 @author: tmahrt
 '''
-from distutils.core import setup
-setup(name='prosodyMorph',
+from setuptools import setup
+import codecs
+setup(name='promo',
       version='1.1.0',
       author='Tim Mahrt',
       author_email='timmahrt@gmail.com',
-      package_dir={'promo':'promo'},
+      url='https://github.com/timmahrt/ProMo',
+      package_dir={'promo': 'promo'},
       packages=['promo',
                 'promo.morph_utils'],
       license='LICENSE',
-      long_description=open('README.rst', 'r').read(),
+      long_description=codecs.open('README.rst', 'r', encoding="utf-8").read(),
 #       install_requires=[], # No requirements! # requires 'from setuptools import setup'
       )
