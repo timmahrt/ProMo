50dd0fda3a86de4bd57e85699b019ad0c0f3509e
Tim Mahrt <timmahrt@gmail.com>
2017-01-27 15:36:59 +0100

    FEATURE: Added code making it easier to manipulate pitch accents
    This comes with an example of how to use it

---
+++ examples/modify_pitch_accent_example.py
@@ -0,0 +1,126 @@
+'''
+Created on Jan 26, 2017
+
+This file contains two examples demonstrating the use
+of the module modify_pitch_accent
+
+@author: Tim
+'''
+
+import os
+from os.path import join
+
+from praatio import tgio
+from praatio import dataio
+from praatio import pitch_and_intensity
+from praatio import praat_scripts
+from praatio import praatio_scripts
+
+from promo.morph_utils import modify_pitch_accent
+
+
+def toStr(inputNum):
+    if inputNum < 0:
+        retStr = '%02d' % inputNum
+    else:
+        retStr = '+%02d' % inputNum
+    
+    return retStr
+
+
+root = os.path.abspath(join('.', 'files'))
+rootOutputPath = join(root, "modify_pitch_accent")
+praatEXE = r"C:\Praat.exe"  # Windows path
+
+
+####################################
+# 1st example - a simple example
+# Create more emphasis on the subject
+####################################
+
+tgFN = "mary1.TextGrid"
+wavFN = "mary1.wav"
+pitchIntensityFN = "mary1.txt"
+originalPitchFN = "mary1.pitch"
+outputWavFN = "mary1_accented.wav"
+outputPitchFN = "mary1_accented.pitch"
+minPitch = 75
+maxPitch = 450
+
+if not os.path.exists(rootOutputPath):
+    os.mkdir(rootOutputPath)
+
+# 1st - get pitch
+piList = pitch_and_intensity.audioToPI(root, wavFN, rootOutputPath,
+                                       pitchIntensityFN,
+                                       praatEXE, minPitch, maxPitch)
+pitchList = [(timeV, pitchV) for timeV, pitchV, _ in piList]
+
+dur = praatio_scripts.getDuration(join(root, wavFN))
+pointObj = dataio.PointObject2D(pitchList, dataio.PITCH, 0, dur)
+pointObj.save(join(rootOutputPath, originalPitchFN))
+
+
+# 2nd - get region to manipulate.  Let's make the subject more emphatic!
+tg = tgio.openTextGrid(join(root, "mary1.TextGrid"))
+tier = tg.tierDict["TokensAlign"]
+start, stop, _ = tier.entryList[0]  # Getting info for the first word
+
+targetPitchList = [(timeV, pitchV) for timeV, pitchV in pitchList
+                   if timeV >= start and timeV <= stop]
+
+# 3rd - make manipulation
+accent = modify_pitch_accent.PitchAccent(targetPitchList)
+accent.addPlateau(0.05)  # Peak is dragged out for 0.05 seconds
+accent.adjustPeakHeight(60)  # Plateau is raised by 60 hz
+accent.shiftaccent(-0.03)  # Recenter the plateau around the original peak
+
+# 4th - integrate results
+moddedPitchList = accent.reintegrate(pitchList)
+
+# 5th - resynthesize
+praat_scripts.resynthesizePitch(praatEXE,
+                                join(root, wavFN),
+                                join(rootOutputPath, outputPitchFN),
+                                join(rootOutputPath, outputWavFN),
+                                minPitch,
+                                maxPitch,
+                                pointList=moddedPitchList)
+
+
+####################################
+# 2nd example - a more complicated example
+# Incrementally create more (or less) emphasis on the subject
+# with a 3 by 3 by 3 matrix of different changes
+####################################
+
+# 1st and 2nd -- already done above
+
+# 3rd - make manipulation
+
+outputFN = join(rootOutputPath,
+                os.path.splitext(outputWavFN)[0] + "_s%s_h%s_p%s")
+    
+for plateauAmount in [0.0, 0.04, 0.08]:
+    for heightAmount in [0, 40, 80]:
+        for shiftAmount in [0.0, 0.04, 0.08]:
+            
+            accent = modify_pitch_accent.PitchAccent(targetPitchList)
+            accent.addPlateau(plateauAmount)
+            accent.adjustPeakHeight(heightAmount)
+            accent.shiftaccent(shiftAmount)
+            
+            # 4th - integrate results
+            moddedPitchList = accent.reintegrate(pitchList)
+            
+            # 5th - resynthesize the wav file
+            outputName = outputFN % (toStr(shiftAmount * 1000),
+                                     toStr(heightAmount),
+                                     toStr(plateauAmount * 1000))
+            wavOutputFN = outputName + ".wav"
+            pitchOutputFN = outputName + ".pitch"
+            praat_scripts.resynthesizePitch(praatEXE, join(root, wavFN),
+                                            pitchOutputFN, wavOutputFN,
+                                            minPitch, maxPitch,
+                                            pointList=moddedPitchList)
+            
---
+++ promo/morph_utils/modify_pitch_accent.py
@@ -0,0 +1,147 @@
+'''
+Created on Jan 26, 2017
+
+This convenience code that makes it easy to manipulate
+characteristics of a pitch accent.  It is flexible in how
+it is used (on a stylized pitch contour or raw values and
+as a single step or in a piecewise manner).
+
+See examples/modify_pitch_accent_example.py for example usage.
+
+NOTE: By pitch accent, I mean a single hill or peak, surrounded
+by less intense values to the left and right.  Some of the
+functions make reference to the max value in the dataset, so if you
+have multiple peaks or a downward slope, etc. you may get strange
+results.
+
+@author: Tim
+'''
+import copy
+    
+    
+def _deletePoints(f0List, start, end):
+    return [(timeV, f0V) for timeV, f0V in f0List
+            if timeV < start or timeV > end]
+
+
+class PitchAccent(object):
+    
+    def __init__(self, pointList):
+        
+        self.pointList = copy.deepcopy(pointList)
+        
+        self.netLeftShift = 0
+        self.netRightShift = 0
+        
+        pitchList = [f0V for _, f0V in self.pointList]
+        minV = min(pitchList)
+        maxV = max(pitchList)
+        
+        self.peakI = pitchList.index(maxV)
+    
+    def adjustPeakHeight(self, heightAmount):
+        '''
+        Adjust peak height
+        
+        The foot of the accent is left unchanged and intermediate
+        values are linearly scaled
+        '''
+        if heightAmount == 0:
+            return
+        
+        pitchList = [f0V for _, f0V in self.pointList]
+        minV = min(pitchList)
+        maxV = max(pitchList)
+        scale = lambda x, y: x + y * (x - minV) / float(maxV - minV)
+        
+        self.pointList = [(timeV, scale(f0V, heightAmount))
+                          for timeV, f0V in self.pointList]
+    
+    def addPlateau(self, plateauAmount, pitchSampFreq=None):
+        '''
+        Add a plateau
+        
+        A negative plateauAmount will move the peak backwards.
+        A positive plateauAmount will move the peak forwards.
+        
+        All points on the side of the peak growth will also get moved.
+        i.e. the slope of the peak does not change.  The accent gets
+        wider instead.
+        
+        If pitchSampFreq=None, the plateau will only be specified by
+        the start and end points of the plateau
+        '''
+        if plateauAmount == 0:
+            return
+        
+        leftSide = self.pointList[:self.peakI]
+        rightSide = self.pointList[self.peakI:]
+        maxPoint = self.pointList[self.peakI]
+        
+        if pitchSampFreq is not None:
+            numSteps = abs(int(plateauAmount / pitchSampFreq))
+            timeChangeList = [stepV * pitchSampFreq
+                              for stepV in
+                              range(0, numSteps + 1)]
+        else:
+            timeChangeList = [plateauAmount, ]
+            
+        # Shift the side being pushed by the plateau
+        if plateauAmount < 0:  # Plateau moves left of the peak
+            plateauPoints = [(maxPoint[0] - timeChange, maxPoint[1])
+                             for timeChange in timeChangeList]
+            leftSide = [(timeV + plateauAmount, f0V)
+                        for timeV, f0V in leftSide]
+            self.netLeftShift += plateauAmount
+            
+        elif plateauAmount > 0:  # Plateau moves right of the peak
+            plateauPoints = [(maxPoint[0] + timeChange, maxPoint[1])
+                             for timeChange in timeChangeList]
+            rightSide = [(timeV + plateauAmount, f0V)
+                         for timeV, f0V in rightSide]
+            self.netRightShift += plateauAmount
+        
+        self.pointList = leftSide + plateauPoints + rightSide
+                   
+    def shiftAccent(self, shiftAmount):
+        '''
+        Move the whole accent earlier or later
+        '''
+        if shiftAmount == 0:
+            return
+        
+        self.pointList = [(time + shiftAmount, pitch)
+                          for time, pitch in self.pointList]
+        
+        # Update shift amounts
+        if shiftAmount < 0:
+            self.netLeftShift += shiftAmount
+        elif shiftAmount >= 0:
+            self.netRightShift += shiftAmount
+    
+    def deleteOverlapping(self, targetList):
+        '''
+        Erase points from another list that overlap with points in this list
+        '''
+        start = self.pointList[0][0]
+        stop = self.pointList[-1][0]
+        
+        if self.netLeftShift < 0:
+            start += self.netLeftShift
+            
+        if self.netRightShift > 0:
+            stop += self.netRightShift
+            
+        targetList = _deletePoints(targetList, start, stop)
+        
+        return targetList
+    
+    def reintegrate(self, fullPointList):
+        '''
+        Integrates the pitch values of the accent into a larger pitch contour
+        '''
+        fullPointList = self.deleteOverlapping(fullPointList)
+        outputPointList = fullPointList + self.pointList
+        outputPointList.sort()
+        
+        return outputPointList
