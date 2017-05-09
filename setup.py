5ab0ec01f67db335a52ce1e309c80fb2078095c6
Tim Mahrt <timmahrt@gmail.com>
2017-05-09 22:42:21 +0200

    BUGFIX: Latest version of praatio required

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Aug 29, 2014
 from setuptools import setup
 import io
 setup(name='promo',
-      version='1.2.3',
+      version='1.2.5',
       author='Tim Mahrt',
       author_email='timmahrt@gmail.com',
       url='https://github.com/timmahrt/ProMo',
