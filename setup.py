dbe4112dd93f024f98cb0ca80837fc70fc5eddd2
Tim Mahrt <timmahrt@gmail.com>
2019-12-08 16:47:27 +0900

    Verifying support for python 3.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Aug 29, 2014
 from setuptools import setup
 import io
 setup(name='promo',
-      version='1.3.0',
+      version='1.3.1',
       author='Tim Mahrt',
       author_email='timmahrt@gmail.com',
       url='https://github.com/timmahrt/ProMo',
