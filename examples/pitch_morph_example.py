9c4a25d0fe4ee5fd6ca37a53fddfed68cd4c148c
Tim Mahrt <timmahrt@gmail.com>
2021-11-20 22:54:00 +0900

    chore: format with black

--- examples/pitch_morph_example.py
+++ examples/pitch_morph_example.py
@@ -1,8 +1,8 @@
-'''
+"""
 
 
 An example of morphing the pitch in one file to that in another
-'''
+"""
 import os
 from os.path import join
 
@@ -12,9 +12,9 @@ from promo import f0_morph
 from promo.morph_utils import utils
 
 # Define the arguments for the code
-root = os.path.abspath(join('.', 'files'))
+root = os.path.abspath(join(".", "files"))
 praatEXE = r"C:\Praat.exe"  # Windows path
-#praatEXE = "/Applications/Praat.app/Contents/MacOS/Praat"  # Mac path
+# praatEXE = "/Applications/Praat.app/Contents/MacOS/Praat"  # Mac path
 
 minPitch = 50
 maxPitch = 350
@@ -33,14 +33,22 @@ toTGFN = join(root, os.path.splitext(toWavFN)[0] + ".TextGrid")
 
 # Prepare the data for morphing
 # 1ST load it into memory
-fromPitch = pitch_and_intensity.extractPI(join(root, fromWavFN),
-                                          join(root, fromPitchFN),
-                                          praatEXE, minPitch,
-                                          maxPitch, forceRegenerate=False)
-toPitch = pitch_and_intensity.extractPI(join(root, toWavFN),
-                                        join(root, toPitchFN),
-                                        praatEXE, minPitch,
-                                        maxPitch, forceRegenerate=False)
+fromPitch = pitch_and_intensity.extractPI(
+    join(root, fromWavFN),
+    join(root, fromPitchFN),
+    praatEXE,
+    minPitch,
+    maxPitch,
+    forceRegenerate=False,
+)
+toPitch = pitch_and_intensity.extractPI(
+    join(root, toWavFN),
+    join(root, toPitchFN),
+    praatEXE,
+    minPitch,
+    maxPitch,
+    forceRegenerate=False,
+)
 
 # 2ND remove intensity values
 fromPitch = [(time, pitch) for time, pitch, _ in fromPitch]
@@ -52,23 +60,28 @@ tierName = "words"
 fromPitch = f0_morph.getPitchForIntervals(fromPitch, fromTGFN, tierName)
 toPitch = f0_morph.getPitchForIntervals(toPitch, toTGFN, tierName)
 
-pitchTier = pitch_and_intensity.extractPitchTier(join(root, fromWavFN),
-                                                 join(root, "mary1.PitchTier"),
-                                                 praatEXE, minPitch,
-                                                 maxPitch,
-                                                 forceRegenerate=True)
+pitchTier = pitch_and_intensity.extractPitchTier(
+    join(root, fromWavFN),
+    join(root, "mary1.PitchTier"),
+    praatEXE,
+    minPitch,
+    maxPitch,
+    forceRegenerate=True,
+)
 # FINALLY: Run the morph process
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName="%s_%s_f0_morph" % (fromName, toName),
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE,
-                 sourcePitchDataList=pitchTier.pointList)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName="%s_%s_f0_morph" % (fromName, toName),
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+    sourcePitchDataList=pitchTier.pointList,
+)
 
 ####################
 # The remaining examples below demonstrate the use of
@@ -79,76 +92,90 @@ f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
 
 
 # Or for more control over the steps:
-stepList = [0.10, ]  # 10% morph
+stepList = [
+    0.10,
+]  # 10% morph
 # Run the morph process
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName="%s_%s_f0_morph" % (fromName, toName),
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName="%s_%s_f0_morph" % (fromName, toName),
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+)
 
 # And, as shown in the next four examples,
 # we can reset the speaker's pitch range and mean pitch back to their own
-stepList = [1.0, ]
+stepList = [
+    1.0,
+]
 
 # Source's mean pitch, target's pitch range
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName="%s_%s_f0_morph_w_average" % (fromName, toName),
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE,
-                 keepPitchRange=False,
-                 keepAveragePitch=True)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName="%s_%s_f0_morph_w_average" % (fromName, toName),
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+    keepPitchRange=False,
+    keepAveragePitch=True,
+)
 
 # Target's mean pitch, source's pitch range
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName="%s_%s_f0_morph_w_range" % (fromName, toName),
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE,
-                 keepPitchRange=True,
-                 keepAveragePitch=False)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName="%s_%s_f0_morph_w_range" % (fromName, toName),
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+    keepPitchRange=True,
+    keepAveragePitch=False,
+)
 
 # Source's mean pitch, source's pitch range
 outputName = "%s_%s_f0_morph_w_range_and_average" % (fromName, toName)
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName=outputName,
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE,
-                 keepPitchRange=True,
-                 keepAveragePitch=True)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName=outputName,
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+    keepPitchRange=True,
+    keepAveragePitch=True,
+)
 
 # Target's mean pitch, target's pitch range (default behavior)
-f0_morph.f0Morph(fromWavFN=join(root, fromWavFN),
-                 pitchPath=root,
-                 stepList=stepList,
-                 outputName="%s_%s_f0_morph_w_regular" % (fromName, toName),
-                 doPlotPitchSteps=True,
-                 fromPitchData=fromPitch,
-                 toPitchData=toPitch,
-                 outputMinPitch=minPitch,
-                 outputMaxPitch=maxPitch,
-                 praatEXE=praatEXE,
-                 keepPitchRange=False,
-                 keepAveragePitch=False)
+f0_morph.f0Morph(
+    fromWavFN=join(root, fromWavFN),
+    pitchPath=root,
+    stepList=stepList,
+    outputName="%s_%s_f0_morph_w_regular" % (fromName, toName),
+    doPlotPitchSteps=True,
+    fromPitchData=fromPitch,
+    toPitchData=toPitch,
+    outputMinPitch=minPitch,
+    outputMaxPitch=maxPitch,
+    praatEXE=praatEXE,
+    keepPitchRange=False,
+    keepAveragePitch=False,
+)
--- promo/morph_utils/plot_morphed_data.py
+++ promo/morph_utils/plot_morphed_data.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Sep 18, 2013
 
 @author: timmahrt
-'''
+"""
 
 import math
 
@@ -14,11 +14,13 @@ except ImportError:
 else:
     hasMatplotlib = True
 
- 
+
 def _matplotlibCheck():
     if not hasMatplotlib:
-        raise ImportError("Matplotlib required to generate plots. "
-                          "Install matplotlib or disable plotting")
+        raise ImportError(
+            "Matplotlib required to generate plots. "
+            "Install matplotlib or disable plotting"
+        )
 
 
 def plotSinglePitchTrack(fromTuple, fnFullPath):
@@ -27,16 +29,19 @@ def plotSinglePitchTrack(fromTuple, fnFullPath):
     fig, (ax0) = plt.subplots(nrows=1)
 
     # Old data
-    plot1 = ax0.plot(fromTuple, color='red', linewidth=2,
-                     )
+    plot1 = ax0.plot(
+        fromTuple,
+        color="red",
+        linewidth=2,
+    )
 
-    plt.ylabel('Pitch (Hz)')
-    plt.xlabel('Sample number')
+    plt.ylabel("Pitch (Hz)")
+    plt.xlabel("Sample number")
 
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-#     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+    #     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
 
-    plt.savefig(fnFullPath, dpi=300, bbox_inches='tight')
+    plt.savefig(fnFullPath, dpi=300, bbox_inches="tight")
     plt.close(fig)
 
 
@@ -46,43 +51,47 @@ def plotTwoPitchTracks(fromTuple, toTuple, fnFullPath):
     fig, (ax0, ax1) = plt.subplots(nrows=2)
 
     # Old data
-    plot1 = ax0.plot(fromTuple, color='red', linewidth=2,
-                     # label="From"
-                     )
+    plot1 = ax0.plot(
+        fromTuple,
+        color="red",
+        linewidth=2,
+        # label="From"
+    )
     ax0.set_title("Mary is going to the mall (statement)")
 
-    plot2 = ax1.plot(toTuple, color='red', linewidth=2,
-                     # label="From"
-                     )
+    plot2 = ax1.plot(
+        toTuple,
+        color="red",
+        linewidth=2,
+        # label="From"
+    )
 
     ax1.set_title("Mary is going to the mall (question)")
 
-    plt.ylabel('Pitch (Hz)')
-    plt.xlabel('Sample number')
+    plt.ylabel("Pitch (Hz)")
+    plt.xlabel("Sample number")
 
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-#     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+    #     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
 
-    plt.savefig(fnFullPath, dpi=300, bbox_inches='tight')
+    plt.savefig(fnFullPath, dpi=300, bbox_inches="tight")
     plt.close(fig)
 
 
 def plotF0(fromTuple, toTuple, mergeTupleList, fnFullPath):
-    '''
+    """
     Plots the original data in a graph above the plot of the dtw'ed data
-    '''
+    """
     _matplotlibCheck()
 
     fig, (ax0) = plt.subplots(nrows=1)
 
     # Old data
-    plot1 = ax0.plot(fromTuple[0], fromTuple[1], color='red',
-                     linewidth=2, label="From")
-    plot2 = ax0.plot(toTuple[0], toTuple[1], color='blue',
-                     linewidth=2, label="To")
+    plot1 = ax0.plot(fromTuple[0], fromTuple[1], color="red", linewidth=2, label="From")
+    plot2 = ax0.plot(toTuple[0], toTuple[1], color="blue", linewidth=2, label="To")
     ax0.set_title("Plot of F0 Morph")
-    plt.ylabel('Pitch (hz)')
-    plt.xlabel('Time (s)')
+    plt.ylabel("Pitch (hz)")
+    plt.xlabel("Time (s)")
 
     # Merge data
     colorValue = 0
@@ -91,27 +100,30 @@ def plotF0(fromTuple, toTuple, mergeTupleList, fnFullPath):
         colorValue += colorStep
         hexValue = "#%02x0000" % int(255 - colorValue)
         if int(colorValue) == 255:
-            ax0.plot(timeList, valueList, color=hexValue, linewidth=1,
-                     label="Merged line, final iteration")
+            ax0.plot(
+                timeList,
+                valueList,
+                color=hexValue,
+                linewidth=1,
+                label="Merged line, final iteration",
+            )
         else:
             ax0.plot(timeList, valueList, color=hexValue, linewidth=1)
 
-    plt.legend(loc=1, borderaxespad=0.)
-#     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
+    plt.legend(loc=1, borderaxespad=0.0)
+    #     plt.legend([plot1, plot2, plot3], ["From", "To", "Merged line"])
 
-    plt.savefig(fnFullPath, dpi=300, bbox_inches='tight')
+    plt.savefig(fnFullPath, dpi=300, bbox_inches="tight")
     plt.close(fig)
 
 
-def plotIntensity(fromDataList, toDataList, mergeTupleList,
-                  controlPoints, fnFullPath):
-
+def plotIntensity(fromDataList, toDataList, mergeTupleList, controlPoints, fnFullPath):
     def mag2DB(valueList):
         stepSize = 500
 
         returnList = []
         for i in range(int(math.ceil(len(valueList) / float(stepSize)))):
-            subList = valueList[i * stepSize:(i + 1) * stepSize]
+            subList = valueList[i * stepSize : (i + 1) * stepSize]
             value = math.sqrt(sum([val ** 2 for val in subList]))
 
             returnList.append(20.0 * math.log10(value))
@@ -127,18 +139,18 @@ def plotIntensity(fromDataList, toDataList, mergeTupleList,
     print(max(toDataList))
 
     fig, ax0 = plt.subplots(nrows=1)
-#     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3)
+    #     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3)
 
     # Old data
-    plot1 = ax0.plot(fromDataList, color='red', linewidth=2, label="From")
-#     plot1.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plot1 = ax0.plot(fromDataList, color="red", linewidth=2, label="From")
+    #     plot1.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
-    plot2 = ax0.plot(toDataList, color='blue', linewidth=2, label="To")
-#     plot2.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plot2 = ax0.plot(toDataList, color="blue", linewidth=2, label="To")
+    #     plot2.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
     ax0.set_title("Plot of Intensity Morph")
-    plt.ylabel('RMS intensity (db)')
-    plt.xlabel('Time (s)')
+    plt.ylabel("RMS intensity (db)")
+    plt.xlabel("Time (s)")
 
     # Merge data
 
@@ -154,36 +166,48 @@ def plotIntensity(fromDataList, toDataList, mergeTupleList,
         hexValue = "#%02x0000" % int(255 - colorValue)
         xValues = [i for i in range(len(valueList))]
         if colorValue == 255.0:
-            plot3 = ax0.plot(xValues, valueList, color=hexValue, linewidth=1,
-                             label="Merged line")
+            plot3 = ax0.plot(
+                xValues, valueList, color=hexValue, linewidth=1, label="Merged line"
+            )
         else:
             plot3 = ax0.plot(xValues, valueList, color=hexValue, linewidth=1)
 
     controlPoints = mag2DB(controlPoints)
     xValues = [i for i in range(len(controlPoints))]
-    ax0.scatter(xValues, controlPoints, color='pink', label="Control points")
+    ax0.scatter(xValues, controlPoints, color="pink", label="Control points")
 
-    plt.legend(loc=1,
-               # bbox_to_anchor=(1.05, 1), loc=2,
-               borderaxespad=0.)
+    plt.legend(
+        loc=1,
+        # bbox_to_anchor=(1.05, 1), loc=2,
+        borderaxespad=0.0,
+    )
 
-    plt.savefig(fnFullPath, dpi=300, bbox_inches='tight')
+    plt.savefig(fnFullPath, dpi=300, bbox_inches="tight")
     plt.close(fig)
 
 
-def plotDuration(fromDurationList, toDurationList, resultDataList,
-                 labelList, fnFullPath):
+def plotDuration(
+    fromDurationList, toDurationList, resultDataList, labelList, fnFullPath
+):
 
     _matplotlibCheck()
 
-    dataList = [fromDurationList, ] + resultDataList + [toDurationList, ]
+    dataList = (
+        [
+            fromDurationList,
+        ]
+        + resultDataList
+        + [
+            toDurationList,
+        ]
+    )
 
     # Sanity check
     for subList in dataList:
-        assert(len(subList) == len(fromDurationList))
+        assert len(subList) == len(fromDurationList)
 
     # Constants
-    colorTuple = ['red', 'yellow', 'blue']
+    colorTuple = ["red", "yellow", "blue"]
     width = 0.2
     n = len(dataList)
     iterN = range(n)
@@ -193,16 +217,24 @@ def plotDuration(fromDurationList, toDurationList, resultDataList,
 
     # Labels
     ax0.set_title("Plot of Duration Morph")
-    plt.ylabel('Duration (s)')
-    plt.xlabel('Iterations')
-    plt.title('Duration morph')
+    plt.ylabel("Duration (s)")
+    plt.xlabel("Iterations")
+    plt.title("Duration morph")
 
     # Draw x ticks (iteration numbers)
     xLabelList = []
     for i in range(len(resultDataList)):
         xLabelList.append(str(i + 1))  # 1 based
 
-    xLabelList = ["From", ] + xLabelList + ["To", ]
+    xLabelList = (
+        [
+            "From",
+        ]
+        + xLabelList
+        + [
+            "To",
+        ]
+    )
     iterN2 = [val + width / 2.0 for val in iterN]
     plt.xticks(iterN2, xLabelList)
 
@@ -216,12 +248,17 @@ def plotDuration(fromDurationList, toDurationList, resultDataList,
 
         # Word label that appears after the final column
         bottomAppend = max(row)
-        ax0.text(n - 1.0 + 0.5, (bottom[0] + (bottomAppend / 3.0)),
-                 labelList[i])
+        ax0.text(n - 1.0 + 0.5, (bottom[0] + (bottomAppend / 3.0)), labelList[i])
 
         # The column data
-        ax0.bar(x=iterN, height=row, bottom=bottom, width=width,
-                alpha=0.6, color=colorTuple[i % 3])
+        ax0.bar(
+            x=iterN,
+            height=row,
+            bottom=bottom,
+            width=width,
+            alpha=0.6,
+            color=colorTuple[i % 3],
+        )
 
         # Update the start positions for the next iteration
 
@@ -231,5 +268,5 @@ def plotDuration(fromDurationList, toDurationList, resultDataList,
     ax0.axhline(bottom[0], color="black")
 
     # Save and cleanup
-    plt.savefig(fnFullPath, dpi=300, bbox_inches='tight')
+    plt.savefig(fnFullPath, dpi=300, bbox_inches="tight")
     plt.close(fig)
